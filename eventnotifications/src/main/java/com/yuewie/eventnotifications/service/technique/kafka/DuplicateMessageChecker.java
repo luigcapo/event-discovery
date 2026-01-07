bf7f409b7413cfca2ebf326e206d65817062d25a
luigcapo <luigicapochichi1996@gmail.com>
2026-01-07 20:32:40 +0100

    Refactor: Passage en repo indépendant pour l'API

--- eventnotifications/src/main/java/com/yuewie/eventnotifications/service/technique/kafka/DuplicateMessageChecker.java
+++
@@ -1,6 +0,0 @@
-package com.yuewie.eventnotifications.service.technique.kafka;
-
-public interface DuplicateMessageChecker {
-    boolean isAlreadyProcessed(String keySet, String deliveryId);
-    void ajouterAuSet(String keySet, String... membres);
-}
--- eventstats/src/main/java/com/yuewie/eventstats/service/technique/kafka/DuplicateMessageChecker.java
+++
@@ -1,6 +0,0 @@
-package com.yuewie.eventstats.service.technique.kafka;
-
-public interface DuplicateMessageChecker {
-    boolean isAlreadyProcessed(String keySet, String deliveryId);
-    void ajouterAuSet(String keySet, String... membres);
-}
